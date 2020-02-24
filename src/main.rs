21165385d5bb4ac9ed2664f9ac240c9b8d11189f
Tim Dévé <timdeve@users.noreply.github.com>
2020-02-24 14:41:32 +0000

    Fixes not being able to broadcast on *nix

--- src/main.rs
+++ src/main.rs
@@ -103,6 +103,9 @@ fn send_message(msg: SetPowerMessage) {
 
 fn send(msg: &[u8]) {
     let socket = net::UdpSocket::bind("0.0.0.0:0").expect("failed to bind host socket");
+    socket
+        .set_broadcast(true)
+        .expect("Couldn't enable broadcast");
 
     match socket.send_to(msg, "192.168.1.255:56700") {
         Err(fail) => println!("failed sending {:?}", fail),
